eb9d54e7f92b12a9003b01c43c7df8ce00083972
Rohit Nambiar <mr.rohitnambiar@gmail.com>
2012-10-26 14:48:26 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GolfScorecard::Application.config.secret_token = '12a7ef98e4918bbac271062590955a99b06332ab9c93c04c1d5fac8f6cdf17a6f25c22298a65332c370c9395e26e56c2f2ed91b3f7f8f5c116c04ce9ec7e9b61'
